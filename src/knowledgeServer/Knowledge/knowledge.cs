30083262e0276927afa44e21620e1c7e47713cf9
Steve Strong <sstrong@apprenticesystems.com>
2016-05-22 15:23:43 -0400

    new knowledge

---
+++ src/knowledgeServer/Knowledge/knowledge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace knowledgeServer.Knowledge
+{
+
+    public class knowledge
+    {
+        public string myName;
+        public string myType;
+
+        public knowledge()
+        {
+            myName = "hello";
+            myType = "knowledge";
+        }
+    }
+}
